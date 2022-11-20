f744c5e449083c40cd3b1234479ba8e3c03910db
aroenai <28711790+aroenai@users.noreply.github.com>
2022-11-19 21:01:10 -0700

    HTML Help Projects - USA
    Uploading initial USA CHM project files

---
+++ HTML Help/USA/NewAliases.h
@@ -0,0 +1,18 @@
+; Alias File written automatically by HHPMod Version 3.2.2
+; November 18, 2022, 01:35 PM
+; Automatic aliases for normal topics ;;;;;;;;;;;;;;;;;;;;;;;;;;
+idh_Contents = html\idh_Contents.htm
+Using_Monopoly_CD_ROM_Menus = html\Using_Monopoly_CD_ROM_Menus.htm
+How_to_Play_the_Game = html\How_to_Play_the_Game.htm
+Troubleshooting = html\Troubleshooting.htm
+Creating_an_AI = html\Creating_an_AI.htm
+Waiting_Room = html\Waiting_Room.htm
+File_Menu = html\File_Menu.htm
+Action_Menu = html\Action_Menu.htm
+Options_Menu = html\Options_Menu.htm
+Help_Menu = html\Help_Menu.htm
+Interrupting_Play = html\Interrupting_Play.htm
+Proposing_a_Trade = html\Proposing_a_Trade.htm
+Starting_a_Game = html\Starting_a_Game.htm
+Chat_Messages = html\Chat_Messages.htm
+Rules = html\Rules.htm
---
+++ HTML Help/USA/popups.js
@@ -0,0 +1,5 @@
+// Javascript Popups File written automatically by HHPMod Version 3.2.2
+// November 18, 2022, 01:35 PM
+
+popfont = "Arial,10,,plain"
+
