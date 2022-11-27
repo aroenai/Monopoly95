10e814c98710d9bc7e9848af057d0bec19899e9d
aroenai <28711790+aroenai@users.noreply.github.com>
2022-11-26 23:40:30 -0700

    HTML Help Projects - UK
    Uploading initial UK CHM project files

---
+++ HTML Help/UK/NewMap.h
@@ -0,0 +1,18 @@
+; Map File written automatically by HHPMod Version 3.2.2
+; November 19, 2022, 09:14 PM
+;
+#define idh_Contents 201
+#define Using_Monopoly_CD_ROM_Menus 202
+#define How_to_Play_the_Game 203
+#define Troubleshooting 204
+#define Creating_an_AI 205
+#define Waiting_Room 206
+#define File_Menu 207
+#define Action_Menu 208
+#define Options_Menu 209
+#define Help_Menu 210
+#define Interrupting_Play 211
+#define Starting_a_Game 213
+#define Chat_Messages 214
+#define Rules 215
+#define Proposing_a_Deal 216
---
+++ HTML Help/UK/popups.js
@@ -0,0 +1,5 @@
+// Javascript Popups File written automatically by HHPMod Version 3.2.2
+// November 19, 2022, 09:14 PM
+
+popfont = "Arial,10,,plain"
+
