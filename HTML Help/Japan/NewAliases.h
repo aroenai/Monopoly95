7712e6360520ec078933df9e71c137d2189e44d5
aroenai <28711790+aroenai@users.noreply.github.com>
2022-12-07 19:13:29 -0700

    HTML Help Projects - Japanese
    Uploading initial Japanese CHM project files

---
+++ HTML Help/Japan/NewAliases.h
@@ -0,0 +1,18 @@
+; Alias File written automatically by HHPMod Version 3.2.2
+; December 07, 2022, 05:42 PM
+; Automatic aliases for normal topics ;;;;;;;;;;;;;;;;;;;;;;;;;;
+Y4I1KJ = html\Y4I1KJ.htm
+25RYS5 = html\25RYS5.htm
+23UQ0YJ = html\23UQ0YJ.htm
+Y7VSA = html\Y7VSA.htm
+2JD_ZFE = html\2JD_ZFE.htm
+KVL.2B = html\KVL.2B.htm
+2WIU0JU = html\2WIU0JU.htm
+6A4JZS = html\6A4JZS.htm
+ANLHUR = html\ANLHUR.htm
+4X_J_.S = html\4X_J_.S.htm
+IW3OG3 = html\IW3OG3.htm
+K8IJWW = html\K8IJWW.htm
+65P75J = html\65P75J.htm
+_G3ZV = html\_G3ZV.htm
+RULES = html\RULES.htm
---
+++ HTML Help/Japan/NewMap.h
@@ -0,0 +1,18 @@
+; Map File written automatically by HHPMod Version 3.2.2
+; December 07, 2022, 05:42 PM
+;
+#define Y4I1KJ 201
+#define 25RYS5 202
+#define 23UQ0YJ 203
+#define Y7VSA 204
+#define 2JD_ZFE 205
+#define KVL.2B 206
+#define 2WIU0JU 207
+#define 6A4JZS 208
+#define ANLHUR 209
+#define 4X_J_.S 210
+#define IW3OG3 211
+#define K8IJWW 212
+#define 65P75J 213
+#define _G3ZV 214
+#define RULES 215
